d3cb3eb491cd312a68536197828fc6e7f56d4ec1
GrapeIsGrape <albertwongyf@gmail.com>
2026-02-25 00:00:55 +0800

    Create Flask App

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
+LOG_FOLDER = os.path.join(os.path.dirname(__file__), 'script_logs')
+
 @app.route("/")
 def home():
-    return render_template("index.html", message="Hello, World!")
+    log_files = []
+    if os.path.exists(LOG_FOLDER):
+        log_files = [f for f in os.listdir(LOG_FOLDER) if os.path.isfile(os.path.join(LOG_FOLDER, f))]
+    return render_template("index.html", log_files=log_files, log_content=None, selected_log=None)
+
+@app.route("/view_log/<logfile>")
+def view_log(logfile):
+    log_files = []
+    if os.path.exists(LOG_FOLDER):
+        log_files = [f for f in os.listdir(LOG_FOLDER) if os.path.isfile(os.path.join(LOG_FOLDER, f))]
+    log_content = ""
+    selected_log = logfile
+    log_path = os.path.join(LOG_FOLDER, logfile)
+    if os.path.exists(log_path):
+        with open(log_path, "r") as f:
+            log_content = f.read()
+    return render_template("index.html", log_files=log_files, log_content=log_content, selected_log=selected_log)
 
 @app.route("/about")
 def about():
